13e3b7ff0d05dc4d4866f33a09a187c3e8a9fb6c
ChiyenLee <chiyenn.lee@gmail.com>
2021-10-26 13:32:06 -0400

    missed on file

--- launch/jetson_launch.jl
+++ launch/jetson_launch.jl
@@ -8,9 +8,9 @@ include("$(@__DIR__)/../nodes/controller/controller_node.jl")
 
 # dummy_node = DummyPublisher.main(; rate=100.0)
 interface_node = HardwareInterface.main(; rate=100.0)
-controller_node = Controller.main(; rate=100.0)
+# controller_node = Controller.main(; rate=100.0)
 
 # dummy_node_task = Threads.@spawn Hg.launch(dummy_node)
 interface_node_task = Threads.@spawn Hg.launch(interface_node)
-controller_task = Threads.@spawn Hg.launch(controller_node) 
+# controller_task = Threads.@spawn Hg.launch(controller_node) 
 
